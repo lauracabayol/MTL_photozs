b98ae2019b8adc8ab0f5fb20e5454afb28ad0ad5
Laura <lauracabayol@gmail.com>
2022-10-26 10:18:19 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+# This requires PyTorch, which cannot be automatically  installed
+# using pip.
+
+setup(
+    name = "MTL_photozs",
+    version = "1.0.0",
+    author = "Laura Cabayol",
+    author_email = "lcabayol@pic.es",
+    description = ("Photometric redshifts with multi-task learning."),
+    keywords = "astronomy",
+    url = "https://github.com/lauracabayol/MTL_photozs",
+    license="GPLv3",
+    packages=['lumos'],
+    install_requires=['numpy', 'pandas', 'torch', 'astropy', 'scipy', 'scikit-image'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Astronomy",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+)
+
+
